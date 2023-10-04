e42ee43e6cb37a01cb338455f9d57817a77f3d57
Priya Yawale <priya.yawale@definelabs.com>
2023-10-04 12:17:13 +0530

    Add chnages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -29,6 +29,21 @@ class CategoriesController < ApplicationController
   end
 
 
+  def edit
+        @category = Category.find(params[:id])
+  end  
+
+  def update
+      @category = Category.find(params[:id])
+       if @category.update(category_params)
+          flash[:notice] = "Category name updated successfully"
+          redirect_to @category
+        else
+             render 'edit'
+       end
+  end
+
+
   private
 
   def category_params
